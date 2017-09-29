dc98d618c66e2b830336e3b3513d6ffeb0d76e5c
SebastianK <sebastian.kopacz@ucalgary.ca>
2017-09-28 23:24:31 -0600

    seperated into multiple files

--- Assignment1/src/boilerplate.h
+++ Assignment1/src/boilerplate.h
@@ -4,6 +4,12 @@
 // Author:  Kamyar Allahverdi, University of Calgary. Minor tweaks by Haysn Hornbeck.
 // Date:    January 2017
 // ==========================================================================
+//
+// Separated into separate files by Sebastian for his sanity.
+//
+
+#ifndef ASSIGNMENT1_BOILERPLATE_H
+#define ASSIGNMENT1_BOILERPLATE_H
 
 #include <iostream>
 #include <fstream>
@@ -23,6 +29,7 @@
 #include <GLFW/glfw3.h>
 
 using namespace std;
+
 
 class Program {
     GLuint vertex_shader;
@@ -189,86 +196,5 @@ public:
     }
 };
 
-void render(Program &program, VertexArray &va) {
-    // clear screen to a dark grey colour
-    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    // Using lines first to get the generator to work
-
-    glUseProgram(program.id);
-    glBindVertexArray(va.id);
-    glDrawArrays(GL_LINES, 0, va.count);
-
-    glBindVertexArray(0);
-    glUseProgram(0);
-
-}
-
-int main(int argc, char *argv[]) {
-    // initialize the GLFW windowing system
-    if (!glfwInit()) {
-        cout << "ERROR: GLFW failed to initialize, TERMINATING" << endl;
-        return -1;
-    }
-    glfwSetErrorCallback([](int error, const char* description) {
-        cout << "GLFW ERROR " << error << ":" << endl;
-        cout << description << endl;
-    });
-
-    // Use OpenGL 4.1
-    GLFWwindow *window = 0;
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    window = glfwCreateWindow(1024, 1024, "CPSC 453 OpenGL Boilerplate", 0, 0);
-    if (!window) {
-        cout << "Program failed to create GLFW window, TERMINATING" << endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-#ifdef _WIN32
-    // Intialize GLEW
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        cout << "glew init failed" << endl;
-        return -1;
-    }
-#endif
-
-    Program mainProgram("data/vertex.glsl", "data/fragment.glsl");
-
-    vector<float> vertexMatrix= { -0.9, -0.9,
-                                  -0.9, 0.9,
-
-                                  -0.9, 0.9,
-                                  0.9, 0.9,
-
-                                  0.9, 0.9,
-                                  0.9, -0.9};
-
-    
-
-    VertexArray verts(6);
-    verts.addBuffer("v", 0, vertexMatrix);
-
-    // run an event-triggered main loop
-    while (!glfwWindowShouldClose(window)) {
-        // render
-        render(mainProgram, verts);
-
-        glfwSwapBuffers(window);
-
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
-    cout << "The End" << endl;
-    return 0;
-}
+#endif //ASSIGNMENT1_BOILERPLATE_H
---
+++ Assignment1/src/main.h
@@ -0,0 +1,13 @@
+//
+// Created by Sebastian on 9/28/2017.
+//
+
+#ifndef ASSIGNMENT1_MAIN_H
+#define ASSIGNMENT1_MAIN_H
+
+#include "boilerplate.h"
+
+int main(int argc, char *argv[]);
+void render(Program &program, VertexArray &va);
+
+#endif //ASSIGNMENT1_MAIN_H
