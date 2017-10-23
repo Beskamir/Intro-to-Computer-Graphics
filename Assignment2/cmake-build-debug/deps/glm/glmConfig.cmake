54ed8c9356a7444ed59f04cb034ec7cb4d26d9e4
SebastianK <sebastian.kopacz@ucalgary.ca>
2017-10-22 18:09:10 -0600

    remove pointless files

--- Assignment2/cmake-build-debug/deps/glm/glmConfig.cmake
+++
@@ -1,6 +0,0 @@
-set(GLM_VERSION "0.9.8")
-set(GLM_INCLUDE_DIRS "D:/Education/University/Current/cpsc453/Assignments/Assignment2/cpsc453_a2/Assignment2/deps/glm")
-
-if (NOT CMAKE_VERSION VERSION_LESS "3.0")
-    include("${CMAKE_CURRENT_LIST_DIR}/glmTargets.cmake")
-endif()
--- Assignment2/cmake-build-debug/deps/glm/test/gtx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Education/University/Current/cpsc453/Assignments/Assignment2/cpsc453_a2/Assignment2/deps/glm/test/gtx
-# Build directory: D:/Education/University/Current/cpsc453/Assignments/Assignment2/cpsc453_a2/Assignment2/cmake-build-debug/deps/glm/test/gtx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
